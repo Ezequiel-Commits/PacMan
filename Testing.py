3a147b66a27728d5c84833c9b6e923ef221aad9c
Ezequiel-Commits <142255412+Ezequiel-Commits@users.noreply.github.com>
2024-12-09 15:43:58 -0500

    feedback from Proshanto

--- Testing.py
+++ Testing.py
@@ -1,7 +1,7 @@
 import turtle 
 
 window  = None
-WINX, WINY = 30,30
+WINX, WINY = 1000,1000
 
 def main():
     def setupWin():
@@ -14,12 +14,17 @@ def main():
         window.bgcolor("white")
 
     setupWin()
+    '''Proshanto's code'''
+    # reset the coordinate system so that the bottom left is (0, 0) and the top right is (50, 50)
+    turtle.setworldcoordinates(0, 0, 50, 50)
 
-    #Definting the game using a 2d list 
-    game = [
-        ["pacMan","empty","empty"],
-        ["empty","empty","empty"],
-        ["empty","empty","empty"]
+    '''Definting the game using a 2d list'''
+    Maze = [
+        #4 by 20 on each axis right now 
+        ["empty","empty","empty","empty"],
+        ["empty","empty","empty","empty"],
+        ["empty","empty","empty","empty"],
+        ["pacMan","empty","empty","empty"]
     ]
 
     #Spawn in the pacMan turtle 
@@ -28,33 +33,56 @@ def main():
 
     """===variables and lists to enable movement==="""
     pacManDirection = None
+    
+    def goingUp():
+        nonlocal pacManDirection
+        pacManDirection = "up"
+    def goingDown():
+        nonlocal pacManDirection
+        pacManDirection = "down"
 
-    pacManLocation = [0,0]
-    pacManSpeed = [1,1]
+    #start in the bottom left corner 
+    pacManLocation = [3,0] #Should equal [-30,-30] in the first goto function
 
-    """===functions to change the direction of the player==="""
-    def pacManDirectionUp():
-        nonlocal pacManDirection
-        pacManDirection = 1
-    def pacManDirectionRight():
-        nonlocal pacManDirection
-        pacManDirection = 2
-    def pacManDirectionDown():
-        nonlocal pacManDirection
-        pacManDirection = 3
-    def pacManDirectionLeft():
-        nonlocal pacManDirection
-        pacManDirection = 4
+    def movePlayer2():
+        nonlocal pacMan, pacManDirection
+        xcoord = 0
+        ycoord = -30
+        if pacManDirection == "up":
+            #Some counterintuitive code here; shouldn't the coordinates connect to the maze 2d list? 
+            ycoord += 20
+            pacMan.goto(xcoord,ycoord)
+            pacManLocation[0] -= 1
+            print(pacManLocation[0])
+            Maze[pacManLocation[0]+1][pacManLocation[1]]="empty" 
+            Maze[pacManLocation[0]][pacManLocation[1]]="pacman"
+            #print(Maze)
+        elif pacManDirection == "down":
+            pass
 
-    def movePlayer():
-        pass
 
     def animate():
-        #clear frame
-        #update the model
-        #render
-        #run the function every 30 milliseconds
-        pass
+        # 1. clear the current frame
+        pacMan.clear()
+
+        # 2. update the model -- i.e. in memory state of the game
+
+        # 3. re-render the next frame
+
+        #implement player movement now
+        movePlayer2()
+        print(pacManDirection)
+
+        # 4. set a timer to call this function again for the next frame
+        window.ontimer(animate,1000) #hardcode some movement before looping it. 
+
+    #animate()
+    pacMan.goto(50, 50)
+
+    """=== player keybinds==="""
+    window.onkeypress(goingUp, key = "Up")
+    window.onkeypress(goingDown, key = "Down")
+    window.listen()
 
     window.mainloop()
 
