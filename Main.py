700d753eb7146cfbf59103eca94e78c76de56e8a
Ezequiel-Commits <142255412+Ezequiel-Commits@users.noreply.github.com>
2025-01-01 15:22:42 -0500

    Implementing player movement?
    I'm still trying to implement step 2 of Proshanto's four steps, and I think adding player movement is next for me. I'm glad that Main is actually Main, while testing is actually for testing.

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ window  = None
 WINX, WINY = 1000,1000
 
 def main():
-    def setupWin():
+    def setupWin(): 
         global window
         # making turtle object
         window = turtle.Screen()
@@ -14,77 +14,53 @@ def main():
         window.bgcolor("white")
 
     setupWin()
-    '''Proshanto's code'''
     # reset the coordinate system so that the bottom left is (0, 0) and the top right is (4, 4)
-    turtle.setworldcoordinates(0, 0, 4, 4)
+    turtle.setworldcoordinates(0, 0, 3, 3) # an opportunity for dry code to be implemented here 
 
     '''Definting the game using a 2d list'''
     Maze = [
         #4 by 4 list to start
+        ["pacMan","empty","empty","empty"],
         ["empty","empty","empty","empty"],
         ["empty","empty","empty","empty"],
-        ["empty","empty","empty","empty"],
-        ["pacMan","empty","empty","empty"]
+        ["empty","empty","empty","empty"]
     ]
 
     #Spawn in the pacMan turtle 
     pacMan = turtle.Turtle()
     pacMan.penup()
 
-    """===variables and lists to enable movement==="""
-    pacManDirection = None
-    
-    def goingUp():
-        nonlocal pacManDirection
-        pacManDirection = "up"
-    def goingDown():
-        nonlocal pacManDirection
-        pacManDirection = "down"
-
-    #start in the bottom left corner 
-    pacManLocation = [0,0] 
-
-    def movePlayer():
-        nonlocal pacMan, pacManDirection
-        xcoord = 0
-        ycoord = -30
-        if pacManDirection == "up":
-            #Some counterintuitive code here; shouldn't the coordinates connect to the maze 2d list? 
-            """
-            ycoord += 20
-            pacMan.goto(xcoord,ycoord)
-            pacManLocation[0] -= 1
-            print(pacManLocation[0])
-            Maze[pacManLocation[0]+1][pacManLocation[1]]="empty" 
-            Maze[pacManLocation[0]][pacManLocation[1]]="pacman" 
-            """
-            #print(Maze)
-        elif pacManDirection == "down":
-            pass
+    def updateModel(startingX,startingY):
+        x1 = startingX 
+        y1 = startingY 
+        print(y1)
+        Maze[x1][y1] = "pacMan"
+        #pretend that the player wants to go up
+        Maze[x1][y1] = "Empty"
+        if y1+1 > 3:
+            window.bye()
+        Maze[x1][y1+1] = "pacMan" 
 
 
-    def animate():
+    def animate(startingX, startingY):
         # 1. clear the current frame(Isn't relevant now.)
         pacMan.clear()
 
         # 2. update the model -- i.e. in memory state of the game via the 2d list
-        if pacManDirection == "up":
-            Maze[pacManLocation[0]][pacManLocation[1]] = "empty"
-            Maze[pacManLocation[0]][pacManLocation[1]] = "pacMan"
-        # 3. re-render the next frame
-
-        #implement player movement now
+        updateModel(startingX, startingY)
+        
+        # 3. render the next frame
+        for x in range(4):
+            for y in range(4):
+                if Maze[x][y] == "pacMan": #how to make the previous location of pacMan into "empty"? 
+                    pacMan.goto(x,y)
+                    pacMan.dot(8)
 
         # 4. set a timer to call this function again for the next frame
-        window.ontimer(animate,1000) #hardcode some movement before looping it. 
+        window.ontimer(animate(startingX,startingY+1),100) #hardcode some movement before looping it. 
 
-    #animate()
-    pacMan.goto(50, 50)
-
-    """=== player keybinds==="""
-    window.onkeypress(goingUp, key = "Up")
-    window.onkeypress(goingDown, key = "Down")
-    window.listen()
+    animate(0,0)
+    #pacMan.goto(0, 2)
 
     window.mainloop()
 
--- Testing.py
+++ Testing.py
@@ -30,38 +30,62 @@ def main():
     pacMan = turtle.Turtle()
     pacMan.penup()
 
-    def movePlayer():
-        nonlocal pacMan
+    pacManDirection = None # a global variable that will allow for player movement 
+
+    """===functions to change the pacManDirection of the player==="""
+    def pacManDirectionUp():
+        nonlocal pacManDirection
+        pacManDirection = 1
+    def pacManDirectionRight():
+        nonlocal pacManDirection
+        pacManDirection = 2
+    def pacManDirectionDown():
+        nonlocal pacManDirection
+        pacManDirection = 3
+    def pacManDirectionLeft():
+        nonlocal pacManDirection
+        pacManDirection = 4
+
+    def updateModel(startingX,startingY):
+        #Update the maze model based on player input 
+        nonlocal pacManDirection
+        x1 = startingX 
+        y1 = startingY 
+        Maze[x1][y1] = "pacMan"
+        print(x1,y1)
+        #Move the player based on their input 
+        Maze[x1][y1] = "Empty"
+        if y1+1 > 3:
+            window.bye()
+        Maze[x1][y1+1] = "pacMan" 
+
 
     def animate(startingX, startingY):
         # 1. clear the current frame(Isn't relevant now.)
         pacMan.clear()
 
         # 2. update the model -- i.e. in memory state of the game via the 2d list
-        x1 = startingX 
-        print(x1)
-        y1 = startingY 
-        print(y1)
-        Maze[x1][y1] = "pacMan"
-        #pretend that the player wants to go up
-        Maze[x1][y1] = "Empty"
-        if y1+1 > 3:
-            window.bye()
-        Maze[x1][y1+1] = "pacMan" # Implementing an if statement to stop the index from going out of range
-        
+        updateModel(startingX, startingY)
         
         # 3. render the next frame
         for x in range(4):
             for y in range(4):
-                if Maze[x][y] == "pacMan": #how to make the previous location of pacMan into "empty"? 
+                if Maze[x][y] == "pacMan": 
+                    print(x,y)
                     pacMan.goto(x,y)
                     pacMan.dot(8)
 
         # 4. set a timer to call this function again for the next frame
-        window.ontimer(animate(startingX,startingY+1),100) #hardcode some movement before looping it. 
+        window.ontimer(animate,10000)
 
     animate(0,0)
-    #pacMan.goto(0, 2)
+
+    """=== player keybinds==="""
+    window.onkeypress(pacManDirectionUp, key = "Up")
+    window.onkeypress(pacManDirectionDown, key = "Down")
+    window.onkeypress(pacManDirectionRight, key = "Right")
+    window.onkeypress(pacManDirectionLeft, key = "Left")
+    window.listen()
 
     window.mainloop()
 
