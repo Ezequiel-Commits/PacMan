0702480b3d642adf31bdf3baf96de550f4246f4c
Ezequiel-Commits <142255412+Ezequiel-Commits@users.noreply.github.com>
2025-01-10 16:12:53 -0500

    Meeting with Proshanto for help
    Proshanto helped me speed up the render speed as well as reduce repetitive code. I'd like to review his edits before moving futher, as it seems I wasn't going about the second step of his framework correctly. I think player movement is next.

--- Testing.py
+++ Testing.py
@@ -15,7 +15,8 @@ def main():
 
     setupWin()
     # reset the coordinate system so that the bottom left is (0, 0) and the top right is (4, 4)
-    turtle.setworldcoordinates(0, 0, 3, 3) # an opportunity for dry code to be implemented here 
+    turtle.setworldcoordinates(0, 0, 4, 4) # an opportunity for dry code to be implemented here 
+    turtle.tracer(False)
 
     '''Definting the game using a 2d list'''
     Maze = [
@@ -25,18 +26,14 @@ def main():
         ["empty","empty","empty","empty"],
         ["empty","empty","empty","empty"]
     ]
-    for x in range(4):
-            for y in range(4):
-                if Maze[x][y] == "pacMan":
-                    pacManX = x
-                    pacManY = y
-                    
+    
 
     #Spawn in the pacMan turtle 
     pacMan = turtle.Turtle()
     pacMan.penup()
+    pacMan.speed(0)
 
-    pacManDirection = None # a global variable that will allow for player movement 
+    pacManDirection = "Up" # a global variable that will allow for player movement 
 
     """===functions to change the pacManDirection of the player==="""
     def pacManDirectionUp():
@@ -54,15 +51,32 @@ def main():
 
     def updateModel():
         #Update the maze model based on player input 
-        nonlocal pacManDirection, pacManX, pacManY
-        print("1"+pacManX,pacManY)
+        nonlocal pacManDirection
+        for x in range(4):
+            for y in range(4):
+                if Maze[x][y] == "pacMan":
+                    pacManX = x
+                    pacManY = y
+
+        # print("1"+pacManX,pacManY)
         if pacManDirection == "Up": 
-            Maze[pacManX][pacManY] = "pacMan"
+            #Maze[pacManX][pacManY] = "pacMan"
             #Move the player based on their input 
             Maze[pacManX][pacManY] = "Empty"
             if pacManY+1 > 3:
                 window.bye()
             Maze[pacManX][pacManY+1] = "pacMan" 
+
+    def render():
+        """Draw the current frame"""
+        nonlocal Maze
+        for x in range(4):
+            for y in range(4):
+                if Maze[x][y] == "pacMan": 
+                    # print("2"+x,y)
+                    pacMan.goto(x,y)
+                    pacMan.dot(30)
+        turtle.update()
 
     def animate():
         # 1. clear the current frame(Isn't relevant now. 1/8)
@@ -72,18 +86,13 @@ def main():
         updateModel()
         
         # 3. render the next frame
-        for x in range(4):
-            for y in range(4):
-                if Maze[x][y] == "pacMan": 
-                    print("2"+x,y)
-                    pacMan.goto(x,y)
-                    pacMan.dot(8)
+        render()
 
         # 4. set a timer to call this function again for the next frame
         window.ontimer(animate,1000)
 
     animate()
-
+    #render()
     """=== player keybinds==="""
     window.onkeypress(pacManDirectionUp, key = "Up")
     window.onkeypress(pacManDirectionDown, key = "Down")
